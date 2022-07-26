631a56295d4a2e2e4ddc4657650643d8737b91ff
sumiati04 <ss6289285@gmail.com>
2022-07-26 11:36:02 +0700

    tes

---
+++ soal2.php
@@ -0,0 +1,14 @@
+function reverseString (str){
+     const arrayStrings = str. str.split("");
+
+     const reverseArray = arrayString.reverse();
+
+     const joinArray = reverseArray.join("");
+
+     return joinArray;
+} 
+
+const string = prompt('enter a string:');
+
+const result = reverseString(string);
+console.log(result);
